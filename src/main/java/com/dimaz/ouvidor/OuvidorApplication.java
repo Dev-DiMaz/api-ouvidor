7ee9c8b8b2e920f97898baaa47a67b99a97ab767
Diogo Mazui <dev.diogo@gmail.com>
2020-11-05 15:42:26 -0300

    fst

---
+++ src/main/java/com/dimaz/ouvidor/OuvidorApplication.java
@@ -0,0 +1,13 @@
+package com.dimaz.ouvidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuvidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OuvidorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dimaz/ouvidor/OuvidorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimaz.ouvidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuvidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
