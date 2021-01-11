51c2c537b91e9b2f861951e136ced82059284753
Diogo Mazui <dev.diogo@gmail.com>
2021-01-11 20:50:17 -0300

    oic

--- src/main/java/com/dimaz/ouvidor/config/KafkaConfig.java
+++ src/main/java/com/dimaz/ouvidor/config/KafkaConfig.java
@@ -5,34 +5,64 @@ import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+
 
 @EnableKafka
 @Configuration
 public class KafkaConfig {
 
-	@Bean
-	public ConsumerFactory<String, String> consumerFactory(){
-		Map<String, Object>config = new HashMap<>();
-		
-		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:8081");
-		config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
-		config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-		config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-		
-		return new DefaultKafkaConsumerFactory<>(config);
-	}
+//	@Bean
+//	public ConsumerFactory<String, String> consumerFactory(){
+//		Map<String, Object>config = new HashMap<>();
+//		
+//		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:8081");
+//		config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
+//		config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+//		config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+//		
+//		return new DefaultKafkaConsumerFactory<>(config);
+//	}
+//	
+//	@Bean
+//	public ConcurrentKafkaListenerContainerFactory concurrentKafkaListenerContainerFactory() {
+//		ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+//		factory.setConsumerFactory(consumerFactory());
+//		
+//		return factory;
+//	}
 	
-	@Bean
-	public ConcurrentKafkaListenerContainerFactory concurrentKafkaListenerContainerFactory() {
-		ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
-		factory.setConsumerFactory(consumerFactory());
-		
-		return factory;
-	}
+	@Autowired
+    KafkaProperties properties;
+
+    @Bean
+    public Map<String, Object> consumerConfigs() {
+        System.out.println("");
+        System.out.println(properties.getUrl());
+        Map<String, Object> config = (Map) properties.buildDefaults();
+        return config;
+
+    }
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<String, String>(consumerConfigs());
+    }
+
+    @Bean
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
 }
